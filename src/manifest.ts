df981a837b0ccf7e93cf14b9682e76077597558f
Leandro Menezes <lelemm@gmail.com>
2024-12-30 09:09:31 -0300

    change plugin name

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,10 +1,10 @@
 import { ActualPluginManifest } from 'plugins-shared';
 
 const manifest: ActualPluginManifest = {
-  url: "https://github.com/actual-plugins/example/",
-  name: "Example Plugin",
-  version: "0.0.3",
-  description: "This is an example plugin.",
+  url: "https://github.com/actual-plugins/dracula-theme/",
+  name: "Dracula theme",
+  version: "0.0.1",
+  description: "Dracula theme for Actual Budget.",
   pluginType: 'client',
   minimumActualVersion: 'v24.12.0',
   author: "Leandro Menezes"
